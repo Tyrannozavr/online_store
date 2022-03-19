25fd73be5c0bd4cb296a75afaf4d95235a5ce3cd
dmiv <dmitriy40647274@gmail.com>
2022-03-19 11:37:50 +0300

    added post method

--- products/views.py
+++ products/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.views.generic import ListView, DetailView
 from . import models
-from . import forms
 import django.forms
 
 class Products(ListView):
@@ -11,24 +10,21 @@ class Products(ListView):
 class ProductsDetail(DetailView):
     model = models.Products
 
+    def post(self, request, *args, **kwargs):
+        if request.POST:
+            print('post')
+            print(request.POST['count'])
+            print(request.POST['discount'])
+        print(args)
+        print(kwargs)
+        return HttpResponse('pass')
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        a = self.get_object()
-        product = models.Products.objects.get(id=a.id)
-        # print(choice.discount.all())
-        form = forms.AllowDiscount
-        # print([i for i in product.discount.all()])
-        # print([(i.name, str(i.discount)) for i in product.discount.all()])
-        form.choices = [(i.name, str(i.discount)) for i in product.discount.all()]
-        # choices = [(i.name, str(i.discount)) for i in choice.discount.all()]
-        # print(choices)
-        # form.choices = choices
-        context['discount'] = form
-        class TwoForm(django.forms.Form):
-            choice = (('1', 'test'),
-                      ('2', 'test'))
+        product = self.get_object()
+        class Buy(django.forms.Form):
             choice = [(i.name, str(i.discount)) for i in product.discount.all()]
-            test = django.forms.ChoiceField(choices=choice)
-        # print(context['discount'].choices)
-        context['form_two'] = TwoForm
+            discount = django.forms.ChoiceField(choices=choice)
+            count = django.forms.IntegerField(min_value=1)
+        context['buy'] = Buy
         return context
